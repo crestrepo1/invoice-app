2d8cab869e53e9aaf00e543b57e11012f4502c9f
Camilo Restrepo <camilo.restrepo@logmein.com>
2020-03-13 08:45:47 -0700

    Updates

---
+++ precache-manifest.ea23f6ea25e0fd687ddeefb83b41c552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb3525c558bb62eef3d737dd78ee9ac",
+    "url": "/invoice-app/index.html"
+  },
+  {
+    "revision": "03e2f14ce3cae5d54ca5",
+    "url": "/invoice-app/static/css/main.c8706603.chunk.css"
+  },
+  {
+    "revision": "db80c689ef6f7e9e1e10",
+    "url": "/invoice-app/static/js/2.33762c92.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/invoice-app/static/js/2.33762c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e2f14ce3cae5d54ca5",
+    "url": "/invoice-app/static/js/main.27a04dfd.chunk.js"
+  },
+  {
+    "revision": "6d59bf0d79d2773ac810",
+    "url": "/invoice-app/static/js/runtime-main.2fd9cb65.js"
+  }
+]);
